7124fd87ea7f676ed5c04d061026081986ebcc58
Deborsi Basu <64161801+debraj330@users.noreply.github.com>
2025-06-25 14:27:16 +0100

    Create ai_control_engine.py

---
+++ ai_control_engine.py
@@ -0,0 +1,23 @@
+import zmq
+
+# Setup
+context = zmq.Context()
+register_socket = context.socket(zmq.REQ)
+register_socket.connect("tcp://192.168.53.11:5559")
+
+def wait_for_node():
+    while True:
+        user_input = input("Do you start the network_node and your network_node is successfully registered to the register.py? (Yes/No): ")
+        if user_input.lower() == "yes":
+            register_socket.send_string("CHECK_NODE")
+            reply = register_socket.recv_string()
+            if reply == "NODE_PRESENT":
+                print("[AI Engine] Network Node is registered. Proceeding with interaction...")
+                break
+            else:
+                print("[AI Engine] No valid network_node present. Waiting again.")
+        else:
+            print("[AI Engine] Waiting for valid network node...")
+
+if __name__ == "__main__":
+    wait_for_node()
