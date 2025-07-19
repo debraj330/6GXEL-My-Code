89f4ed7d9deddfbed37a1758175ce92415656035
Deborsi Basu <64161801+debraj330@users.noreply.github.com>
2025-07-19 12:25:52 +0100

    Update ai_control_engine1.py

--- ai_control_engine1.py
+++ ai_control_engine1.py
@@ -8,7 +8,7 @@ AI_ID = "AI001"
 def register_ai():
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
-    socket.connect("tcp://192.168.0.178:5559")
+    socket.connect("tcp://192.168.0.178:5561")
 
     # Step 1: Send only registration
     socket.send_json({
@@ -25,10 +25,27 @@ def register_ai():
 
     return False
 
+def send_command(app_command):
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://192.168.0.178:5561")
+    
+    socket.send_json({
+        "ai_id": AI_ID,
+        "command": app_command
+    })
+
+    try:
+        reply = socket.recv_json()
+        print(f"[AI Engine] Register response: {reply}")
+    except zmq.ZMQError as e:
+        print(f"[AI Engine] ERROR: {e}")
+
 if __name__ == "__main__":
     print("[AI Engine] Running...")
-
-    if register_ai():
-        print("[AI Engine] Registered. Waiting for further instructions in broker...")
-    else:
-        print("[AI Engine] Registration failed.")
+    while True:
+        cmd = input("Enter command for App1 or App2 (APP1/APP2): ").strip()
+        if cmd in ["APP1", "APP2"]:
+            send_command(cmd)
+        else:
+            print("Invalid input. Try again.")
